6fe487ddcc4b16163ac8eadeb004182bf078bb38
sogore <massire.org@gmail.com>
2022-09-16 20:00:15 +0200

    remplacement de entityManagerInterface par EventRepository

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -3,17 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\EventRegistry;
 
 class EventsController extends AbstractController
 {
     #[Route('/events')]
-    public function index(EntityManagerInterface $entityManagerInterface): Response
+    public function index(EventRepository $eventRepository): Response
     {
-        $events = $entityManagerInterface->getRepository(Event::class)->findAll();
+        $events = $eventRepository->findAll();
 
         return $this->render('/events/index.html.twig', compact('events'));
     }
